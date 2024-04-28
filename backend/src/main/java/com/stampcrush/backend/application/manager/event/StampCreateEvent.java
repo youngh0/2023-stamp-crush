918809135a52af0164d9fd0ebb2b901ed524f829
Youngho <cyh6099@gmail.com>
2024-04-29 01:20:56 +0900

    feat: 스탬프 생성 이벤트 식별자 추가

--- backend/src/main/java/com/stampcrush/backend/application/manager/event/StampCreateEvent.java
+++ backend/src/main/java/com/stampcrush/backend/application/manager/event/StampCreateEvent.java
@@ -2,6 +2,7 @@ package com.stampcrush.backend.application.manager.event;
 
 import com.stampcrush.backend.entity.cafe.Cafe;
 import com.stampcrush.backend.entity.user.Customer;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import okhttp3.Cache;
@@ -10,6 +11,7 @@ import okhttp3.Cache;
 @RequiredArgsConstructor
 public class StampCreateEvent {
 
+    private final UUID eventId;
     private final Cafe cafe;
     private final Customer customer;
     private final int stampCount;
